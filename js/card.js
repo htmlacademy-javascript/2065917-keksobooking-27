166b8de88c072b3354413a75dc4429b5a4113e89
constoner <constantin.kalinin@gmail.com>
2022-10-22 21:00:59 +0300

    доделал генератор объявлений: валидация данных срабатывает до заполнения новых узлов. валидация осущетсвляется универсально для всех данных

--- js/card.js
+++ js/card.js
@@ -1,16 +1,11 @@
 import {getFewAdvt} from './data.js';
 
-// функция удаляет узел с пустыми входными данными
-const hideEmpty = (node, source) => {
-  if (
-    ['', 0, null, undefined, Infinity].some((item) => item === source) || Number.isNaN(source)
-  ) {
-    node.remove();
-  }
-};
+// массив генерируемых объектов
+const ADVT_QUANTITY = 1;
+const cardArray = getFewAdvt(ADVT_QUANTITY);
 
 // словарь для отрисовки карточек
-const HOUSING_TYPE_VALUES = {
+const HOUSING_TYPE_DICTIONARY = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
   house: 'Дом',
@@ -18,59 +13,89 @@ const HOUSING_TYPE_VALUES = {
   hotel: 'Отель',
 };
 
-// подготовка шаблона
+// подготовка шаблона карточки
 const mapCanvas = document.querySelector('#map-canvas'); // временный блок с картой
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup'); // шаблон объявления
+const cardArrayFragment = document.createDocumentFragment(); // фрагмент для рендеринга
 
-// количество генерируемых объектов объявлений
-const ADVT_QUANTITY = 10;
-const cardArray = getFewAdvt(ADVT_QUANTITY);
+// функция отрисовки карточки по шаблону и данным из объекта
+const getNewCard = ({author, offer}) => {
+  const cardNode = cardTemplate.cloneNode(true);
 
-const cardArrayFragment = document.createDocumentFragment();
+  // обязательные поля
+  cardNode.querySelector('.popup__title').textContent = offer.title;
+  cardNode.querySelector('.popup__text--address').textContent = offer.address;
+  cardNode.querySelector('.popup__text--price').textContent = `${offer.price}`;
+  cardNode.querySelector('.popup__text--price').innerHTML += ' <span>₽/ночь</span>';
+  cardNode.querySelector('.popup__type').textContent = HOUSING_TYPE_DICTIONARY[offer.type];
+  cardNode.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  cardNode.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
 
-cardArray.forEach(({author, offer}) => {
-  const cardNew = cardTemplate.cloneNode(true);
+  // необязательные поля
+  const optionalNodes = {
+    avatar: cardNode.querySelector('.popup__avatar'),
+    features: cardNode.querySelector('.popup__features'),
+    description: cardNode.querySelector('.popup__description'),
+    photos: cardNode.querySelector('.popup__photos'),
+  };
 
-  // текстовые данные
-  cardNew.querySelector('.popup__avatar').src = author.avatar;
-  cardNew.querySelector('.popup__title').textContent = offer.title;
-  cardNew.querySelector('.popup__text--address').textContent = offer.address;
-  cardNew.querySelector('.popup__text--price').textContent = `${offer.price} `;
-  cardNew.querySelector('.popup__text--price').innerHTML += '<span>₽/ночь</span>';
-  cardNew.querySelector('.popup__type').textContent = HOUSING_TYPE_VALUES[offer.type];
-  cardNew.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  cardNew.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  // данные необязятаельных полей
+  const optionalValues = {
+    avatar: author.avatar,
+    features: offer.features,
+    description: offer.description,
+    photos:offer.photos
+  };
 
-  const cardDescription = cardNew.querySelector('.popup__description');
-  cardDescription.textContent = offer.description;
-  hideEmpty(cardDescription, offer.description);
+  // методы для заполнения необязательных полей
+  const nodeCallback = {
+    avatar: () => {
+      cardNode.querySelector('.popup__avatar').src = optionalValues.avatar;
+    },
 
-  // массив фич
-  const featureList = cardNew.querySelectorAll('.popup__feature');
-  const featureArray = offer.features.map((featureItem) => `popup__feature--${featureItem}`);
+    features: () => {
+      const featureList = cardNode.querySelectorAll('.popup__feature');
+      const featureArray = optionalValues.features.map((featureItem) => `popup__feature--${featureItem}`);
+      featureList.forEach((listItem) => {
+        const feature = listItem.classList[1];
+        if (!featureArray.includes(feature)) {
+          listItem.remove();
+        }
+      });
+    },
 
-  featureList.forEach((listItem) => {
-    const feature = listItem.classList[1];
+    description: () => {
+      cardNode.querySelector('.popup__description').textContent = optionalValues.description;
+    },
 
-    if (!featureArray.includes(feature)) {
-      listItem.remove();
+    photos: () => {
+      const photoTemplate = cardNode.querySelector('.popup__photo');
+      const photosNode = cardNode.querySelector('.popup__photos');
+      photosNode.removeChild(photosNode.lastElementChild);
+      optionalValues.photos.forEach((photoSource) => {
+        const newPhoto = photoTemplate.cloneNode(false);
+        newPhoto.src = photoSource;
+        photosNode.appendChild(newPhoto);
+      });
+    },
+  };
+
+  // заполение необязательных данных с проверкой на отсутсвие
+  Object.keys(optionalValues).forEach((item) => {
+    if (
+      ['', 0, null, undefined, Infinity].some((v) => v === optionalValues[item]) || Number.isNaN(optionalValues[item])
+    ) {
+      optionalNodes[item].remove();
+    } else {
+      nodeCallback[item]();
     }
   });
 
-  // массив фотографий
-  const photosBlock = cardNew.querySelector('.popup__photos');
-  const photoTemplate = cardNew.querySelector('.popup__photo');
+  return cardNode;
+};
 
-  photosBlock.removeChild(photosBlock.lastElementChild);
-
-  offer.photos.forEach((photoSource) => {
-    const newPhoto = photoTemplate.cloneNode(false);
-    newPhoto.src = photoSource;
-    photosBlock.appendChild(newPhoto);
-  });
-
-  cardArrayFragment.appendChild(cardNew);
-});
+// заполнение фрагмента карточками из массива объектов
+cardArray.forEach((item) => cardArrayFragment.appendChild(getNewCard(item)));
 
 // добавление объявлений на карту
 mapCanvas.appendChild(cardArrayFragment);
