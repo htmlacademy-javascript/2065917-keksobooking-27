58ea922918e1c5f770216355a29d4362674a3726
constoner <constantin.kalinin@gmail.com>
2022-10-25 22:57:48 +0300

    переименовал параметр функций

--- js/form.js
+++ js/form.js
@@ -7,19 +7,19 @@ const mapFiltersOptions = mapFiltersForm.querySelectorAll('fieldset, select'); /
 // Слайдер также должен быть заблокирован
 
 // функция для отключения форм
-const disableForm = (formNode, nodeList) => {
+const disableForm = (formNode, fieldsetList) => {
   formNode.classList.add(`${formNode.classList[0]}--disabled`);
 
-  nodeList.forEach((nodeItem) => {
+  fieldsetList.forEach((nodeItem) => {
     nodeItem.disabled = true;
   });
 };
 
 // функции для включения форм
-const enableForm = (formNode, nodeList) => {
+const enableForm = (formNode, fieldsetList) => {
   formNode.classList.remove(`${formNode.classList[0]}--disabled`);
 
-  nodeList.forEach((nodeItem) => {
+  fieldsetList.forEach((nodeItem) => {
     nodeItem.disabled = false;
   });
 };
