0586a99bf518d7f8ab0a0636b36a82c5eadadf50
constoner <constantin.kalinin@gmail.com>
2022-10-25 22:53:59 +0300

    перенес вызовы функций из form в main

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 import './card.js';
 import './form.js';
+
+import {
+  noticeForm,
+  noticeFieldSets,
+  mapFiltersForm,
+  mapFiltersOptions,
+  disableForm,
+  enableForm, // eslint-disable-line
+} from './form.js';
+
+// отключение форм по-умолчанию
+disableForm(noticeForm, noticeFieldSets);
+disableForm(mapFiltersForm, mapFiltersOptions);
+
+// включение форм после загрузки карты
+// enableForm(noticeForm, noticeFieldSets);
+// enableForm(mapFiltersForm, mapFiltersOptions);
+
