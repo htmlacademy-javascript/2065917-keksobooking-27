d940d5767652cc5eca0083cebc6831e80f4abe66
constoner <constantin.kalinin@gmail.com>
2022-10-26 19:54:47 +0300

    правка разбивки на модули

--- js/main.js
+++ js/main.js
@@ -13,13 +13,17 @@ cardArray.forEach((item) => cardArrayFragment.appendChild(getNewCard(item)));
 mapCanvas.appendChild(cardArrayFragment);
 
 import {
-  noticeForm,
-  noticeFieldSets,
-  mapFiltersForm,
-  mapFiltersOptions,
   disableForm,
   enableForm, // eslint-disable-line
 } from './form.js';
+
+// ОТКЛЮЧЕНИЕ ФОРМ НА ВРЕМЯ ЗАГРУЗКИ КАРТЫ
+
+const noticeForm = document.querySelector('.ad-form'); // форма подачи объявления
+const noticeFieldSets = noticeForm.querySelectorAll('fieldset'); // филдсеты формы подачи объявления
+const mapFiltersForm = document.querySelector('.map__filters'); // форма фильтров
+const mapFiltersOptions = mapFiltersForm.querySelectorAll('fieldset, select'); // фильтры для карты
+// Слайдер также должен быть заблокирован
 
 // отключение форм по-умолчанию
 disableForm(noticeForm, noticeFieldSets);
