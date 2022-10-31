08a206114b8e2540e0aa39196b2070da28ddbd30
constoner <constantin.kalinin@gmail.com>
2022-10-31 21:00:04 +0300

    сделал функцию универсальней

--- js/form.js
+++ js/form.js
@@ -1,22 +1,10 @@
 // функция переключения состояния форм
 
-// const toggleFormMode = (formNode, fieldsetList) => {
-//   formNode.classList.toggle(`${formNode.classList[0]}--disabled`);
-
-//   fieldsetList.forEach((nodeItem) => {
-//     nodeItem.disabled = !nodeItem.disabled;
-//   });
-// };
-
-// export {toggleFormMode};
-
-
 const toggleFormMode = (formNode) => {
   formNode.classList.toggle(`${formNode.classList[0]}--disabled`);
+  Array.from(formNode.children).forEach((field) => {
+    field.disabled = !field.disabled;
+  });
 };
 
-const toggleFieldsetMode = (fieldsetItem) => {
-  fieldsetItem.disabled = !fieldsetItem.disabled;
-};
-
-export {toggleFormMode, toggleFieldsetMode};
+export {toggleFormMode};
