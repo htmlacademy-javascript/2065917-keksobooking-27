66162baf70f8de2822367cdce12f9aa1ee47e502
Keks <keks@htmlacademy.ru>
2022-11-07 23:06:17 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,7 +1,19 @@
-import './card.js';
-import './form.js';
-
+import {getAdvertismentArray} from './data.js';
+import {getNewCard} from './card.js';
 import {toggleFormMode} from './form.js';
+
+// ДОБАВЛЕНИЕ КАРТОЧЕК НА КАРТУ
+
+// массив генерируемых объектов
+const ADVERTISMENT_QUANTITY = 1;
+const cardArray = getAdvertismentArray(ADVERTISMENT_QUANTITY);
+
+// рендеринг карточек
+const mapCanvas = document.querySelector('#map-canvas');
+const cardArrayFragment = document.createDocumentFragment();
+
+cardArray.forEach((item) => cardArrayFragment.appendChild(getNewCard(item)));
+mapCanvas.appendChild(cardArrayFragment);
 
 // ОТКЛЮЧЕНИЕ ФОРМ НА ВРЕМЯ ЗАГРУЗКИ КАРТЫ
 
@@ -13,4 +25,5 @@ forms.forEach((form) => toggleFormMode(form));
 
 // включение форм после загрузки карты
 forms.forEach((form) => toggleFormMode(form));
+
 
